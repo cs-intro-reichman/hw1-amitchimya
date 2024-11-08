82d21d54eee1901667b01ac856e190add6d7f252
Amit Chimya <amitchimya@Amits-MacBook-Pro.local>
2024-11-08 17:54:57 +0200

    Trying to upload all the files

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,4 +1,7 @@
 // Represents the hh:mm time format using an AM/PM format. 
+
+import javax.print.DocFlavor.STRING;
+
 public class TimeFormat {
 	public static void main(String[] args) {
 		// In Java, the command-line arguments args[0], args[1], ... are represented
@@ -12,6 +15,21 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		String hDig = "";
+		String format = "";
+        if (hours<12) {
+			hDig = hours + ":";
+			format = " AM";
+		}
+		else {
+			hDig = (hours-12) + ":";
+			format = " PM";
+		}
+		if (minutes<10) {
+			System.out.println(hDig + "0" + minutes + format);
+		}
+		else {
+			System.out.println(hDig + minutes + format);
+		}
 	}
 }
