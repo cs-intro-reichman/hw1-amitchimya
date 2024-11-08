718cefec2da4f385671fea570160b4f112494fd2
Amit Chimya <amitchimya@Amits-MacBook-Pro.local>
2024-11-08 18:54:14 +0200

    Spelling mistake

--- NumWords.java
+++ NumWords.java
@@ -2,11 +2,11 @@
 public class NumWords {
 	public static void main(String args[]) {
 	  int number = Integer.parseInt(args[0]);
-	  int hundrends = number / 100;
-	  int rest = number-(hundrends*100);
+	  int hundreds = number / 100;
+	  int rest = number-(hundreds*100);
 	  int tens = rest/10;
 	  int ones = rest-(tens*10);
 
-	  System.out.println(hundrends + " hundrends, " + tens + " tens, and " + ones + " ones.");
+	  System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
